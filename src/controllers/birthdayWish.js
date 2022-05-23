053fd0798b368b044ab210b906d5b1fa9b667d27
nkbtemmy <emmanuelnkubito2@gmail.com>
2022-05-23 15:44:52 +0200

    making same changes

--- src/controllers/birthdayWish.js
+++ src/controllers/birthdayWish.js
@@ -4,25 +4,29 @@ const birtdayWish = (req,res) =>{
     const dateOfDay = new Date();
     const today = dateOfDay.getDate();
     const thisMonth = dateOfDay.getMonth();
+    console.log("hellowe!!!")
     axios.get("https://voicetoworld.herokuapp.com/api/v1/users/classmates")
     .then((resp)=>{
 
         let arr = resp.data.result;
         // console.log(arr)
         //   sendEmail(data);
+         //if (arr.length === 0) console.log("no one have bd today")
+         let userData = []
          arr.map((data)=>{
             let birthdate = new Date(data.birthdate);
             let bornMonth = birthdate.getMonth();
             let birthday = birthdate.getDate();
             //console.log("bd:",birthday,"bm:",bornMonth)
-            
             if(bornMonth == thisMonth && birthday == today){
+                userData.push(data)
                 console.log(`today is ${data.firstname}  birthday`)
                 console.log(data)
                 sendEmail(data);
             }
             //console.log("no one have bd today")
         })
+        if (userData.length === 0) console.log("no one have bd today")
     // console.log(resp.data.result)
     }).catch((error)=>{
         console.log(error.message)
